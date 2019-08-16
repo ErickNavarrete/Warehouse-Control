8479bac77fb69f04b78f77b57d2326dc1e59486d
Erick Dylan Navarrete de la Vega <ericknavarrete55@gmail.com>
2019-08-15 23:53:11 -0500

    Models and Database Configuration

---
+++ Warehouse-Control/Connection/ConnectionDB.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Warehouse_Control.Forms;
+using Warehouse_Control.Models;
+using Warehouse_Control.Properties;
+
+namespace Warehouse_Control.Connection
+{
+    public class ConnectionDB : DbContext
+    {
+        public ConnectionDB() : base(Settings.Default.Connection)
+        {
+        }
+
+        public virtual DbSet<Departure> Departure { get; set; }
+        public virtual DbSet<DepartureDet> DepartureDet { get; set; }
+        public virtual DbSet<Inventory> Inventory { get; set; }
+        public virtual DbSet<Items> Items { get; set; }
+        public virtual DbSet<Users> Users { get; set; }
+        public virtual DbSet<Warehouse> Warehouse { get; set; }
+    }
+}
